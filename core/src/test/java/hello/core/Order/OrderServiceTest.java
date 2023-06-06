dc918ab24113fad86104a4675a19796d8d76ac96
PARK9898 <wjddjs21@naver.com>
2023-06-06 16:46:12 +0900

    "주문 서비스 테스트"

--- core/src/test/java/hello/core/Order/OrderServiceTest.java
+++ core/src/test/java/hello/core/Order/OrderServiceTest.java
@@ -1,10 +1,10 @@
 package hello.core.Order;
 
-import java.awt.*;
-
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -12,9 +12,15 @@ import hello.core.member.MemberServiceImpl;
 
 public class OrderServiceTest {
 
-	MemberService memberService = new MemberServiceImpl();
-	OrderService orderService = new OrderServiceImpl();
+	MemberService memberService;
+	OrderService orderService;
 
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+		orderService = appConfig.orderService();
+	}
 	@Test
 	void createOrder(){
 		Long memberId = 1L;
