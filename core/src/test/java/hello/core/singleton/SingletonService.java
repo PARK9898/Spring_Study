bcb4610bd59582836fdd1506a873751a44aa99dc
PARK9898 <wjddjs21@naver.com>
2023-06-08 23:41:52 +0900

    싱글톤 서비스 예제

---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,11 @@
+package hello.core.singleton;
+
+public class SingletonService {
+	//static 영역에 하나만 올라감
+	private static final SingletonService instance = new SingletonService();
+
+	public static SingletonService getInstance() {
+		return instance;
+	}
+	
+}
