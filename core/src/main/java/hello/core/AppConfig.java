349dea24e14f49735bce7f4465233fb72e679108
PARK9898 <wjddjs21@naver.com>
2023-06-29 20:10:55 +0900

    "appconfig 수정"

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
 import hello.core.discount.DiscountPolicy;
@@ -10,21 +13,24 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
-
+@Configuration
 public class AppConfig {
     //커멘드 + e 이전 히스토리 보기
+	@Bean
 	public MemberService memberService(){
 		return new MemberServiceImpl(memberRepository());
 	}  //AppConfig을 통해서 MemberService를 불러다 씀
-
-	private MemberRepository memberRepository() {
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService(){
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy(){
 		//return new FixDiscountPolicy();
 		return new RateDiscountPolicy();
