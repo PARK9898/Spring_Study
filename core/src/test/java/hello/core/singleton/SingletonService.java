4c5821b303f034f921deb9a5917a376611b7e9b5
PARK9898 <wjddjs21@naver.com>
2023-07-14 16:06:04 +0900

    "싱글톤 서비스"

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,11 +1,21 @@
 package hello.core.singleton;
 
-public class SingletonService {
-	//static 영역에 하나만 올라감
-	private static final SingletonService instance = new SingletonService();
+import java.sql.SQLOutput;
 
+public class SingletonService {
+	//static 영역에 객체를 딱 1개만 생성해둔다 실행될때 하나만 생성해서 인스턴스에 넣어둔다
+	private static final SingletonService instance = new SingletonService();
+	//조회할때는 밑에서 호출하면 된다
 	public static SingletonService getInstance() {
 		return instance;
+	}
+	//private 생성자로 밖에서 new로 생성하는것을 막아준다 정말 중요한것임
+	private SingletonService() {
+
+	}
+
+	public void logic() {
+		System.out.println("싱글톤 객체 로직 호출");
 	}
 
 }
