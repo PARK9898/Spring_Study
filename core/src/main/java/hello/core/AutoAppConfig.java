9cb4559e9bf0acb4ef7e466582561bc0018b1c1b
PARK9898 <wjddjs21@naver.com>
2023-07-14 16:07:32 +0900

    "Auto Configuration"

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+@Configuration
+@ComponentScan(
+	basePackages = "hello.core" , //이 패키지부터 밑으로 컴포넌트 스캔을 해준다.
+	basePackageClasses = AutoAppConfig.class , //지정하지 않으면 하위패키지 다 뒤진다
+	excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION , classes = Configuration.class)
+)
+public class AutoAppConfig {
+	//@Bean(name = "memoryMemberRepository")
+	//public MemberRepository memberRepository() {
+	//	return new MemoryMemberRepository();
+	//} // 자동 빈 등록과 수동 빈 등록이 충돌하면 수동 빈 등록이 우선권을 가진다 하지만 스프링은 이제 오류가 나도록 하게 한다
+	//@bean으로 등록한 클래스가 하나도 없다
+}
