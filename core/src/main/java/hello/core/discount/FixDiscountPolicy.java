7dcd6c37a3cd7494443d9cda63bb8bf12906dd20
PARK9898 <wjddjs21@naver.com>
2023-08-12 20:14:37 +0900

    할인 정책 수정

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,12 @@
 package hello.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
-
+@Component // Bean is mismatching NoUniqueBeanException
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
 	private int discountFixAmount = 1000;
